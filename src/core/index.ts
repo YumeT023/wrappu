b7ee31abdc2f6c31bc2493b3101917c81bc360e9
Yume <hei.hajatiana@gmail.com>
2023-03-07 08:50:54 +0300

    chore: MIT LICENSE

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,2 +1,9 @@
+/**
+ * @license
+ * Copyright Yumii Saiko All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/YumeT023/wrappu/blob/main/LICENSE
+ */
 export * from "./Cli";
 export * from "./Cmd";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-// TODO: missing tests
+/**
+ * @license
+ * Copyright Yumii Saiko All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/YumeT023/wrappu/blob/main/LICENSE
+ */
+
+/**
+ * @module
+ * @description
+ * Entry point for all APIs of this package
+ */
 export * from "./core";
